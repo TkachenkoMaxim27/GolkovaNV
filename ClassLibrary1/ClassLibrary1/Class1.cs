61de710897ad1ae0e6b038b96fabce56074ce944
TkachenkoMaxim27 <162081857+TkachenkoMaxim27@users.noreply.github.com>
2024-03-04 13:18:32 +0700

    Add files via upload

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robot
+{
+    public class RobotMove
+    {
+        private string name;
+        private (int, int) coordinates;
+        string t;
+
+        public RobotMove(string name, int x, int y)
+        {
+            this.name = name;
+            this.coordinates = (x, y);
+
+        }
+        public void Move()
+        {
+
+            Random rnd = new Random();
+            int direction = rnd.Next(0, 4);
+            switch (direction)
+            {
+                case 0:
+
+                    this.coordinates.Item2++;
+                    this.t = "Вперед";
+
+                    break;
+
+                case 1:
+
+                    this.coordinates.Item1++;
+                    this.t = "Вправо";
+                    break;
+
+                case 2:
+
+                    this.coordinates.Item2--;
+                    this.t = "Назад";
+                    break;
+
+                case 3:
+
+                    this.coordinates.Item1--;
+                    this.t = "Влево";
+                    break;
+            }
+
+        }
+
+        public string GetInfo()
+        {
+            return $"Робот {this.name} {t} ({this.coordinates.Item1}, {this.coordinates.Item2})";
+        }
+    }
+}
---
+++ ClassLibrary1/Robot/Program.cs
@@ -0,0 +1,29 @@
+﻿using Robot;
+namespace ConsoleRobot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Robot.RobotMove robot = new Robot.RobotMove("Робот 1", 2, 0);
+            Robot.RobotMove robot2 = new Robot.RobotMove("Робот 2", 0, 1);
+            Robot.RobotMove robot3 = new Robot.RobotMove("Робот 3", 1, 3);
+
+
+
+            for (int i = 0; i <= 5; i++)
+            {
+                robot.Move();
+                robot2.Move();
+                robot3.Move();
+                Console.WriteLine(robot.GetInfo());
+                Console.WriteLine(robot2.GetInfo());
+                Console.WriteLine(robot3.GetInfo());
+                Console.WriteLine("ПОТОК ЗАВЕРШЕН");
+                Thread.Sleep(2000);
+            }
+
+
+        }
+    }
+}
